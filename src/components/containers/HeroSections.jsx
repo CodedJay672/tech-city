17c102e158cc3561fdbeb74c1242bf6ccb22ad04
codedjay672 <emmajoel6456@gmail.com>
2025-03-08 19:35:30 +0100

    reimplement the Hero section with mobile responsiveness

--- src/components/containers/HeroSections.jsx
+++ src/components/containers/HeroSections.jsx
@@ -3,22 +3,47 @@ import Navbar from "./Navbar";
 
 const HeroSections = () => {
   return (
-    <section className="w-full h-screen relative mb-10 overflow-hidden">
+    <section className="w-full h-screen flex flex-col">
       <Navbar />
-      <div className="w-full relative overflow-hidden">
-        <div className="absolute top-40 left-[40%] flex justify-center items-center flex-col">
-          <h1 className="text-blue-800 font-light text-8xl">
-            Tech <span className="font-bold block -mt-6">City</span>
-          </h1>
-          <p className="text-2xl font-semibold text-blue-900 leading-12">
-            Nigeria's Silicon Valley
-          </p>
+      <div className="flex-1 flex flex-col lg:flex-row  gap-6">
+        <div className="w-full p-6 lg:p-12 flex flex-col gap-6 justify-center items-start">
+          <div className="flex flex-col gap-6">
+            <h1 className="text-blue-950 font-semibold text-4xl lg:text-5xl">
+              LET YOUR MONEY DO THE WORK. ENJOY THE OPPORTUNITY.
+            </h1>
+            <p className="text-base lg:text-lg font-medium lowercase">
+              INVEST IN REAL ESTATE LET YOUR MONEY DO THE WORK. ENJOY THE
+              OPPORTUNITY TO INCREASE YOUR SAVED INCOME BY 38% IN JUST ONE YEAR
+              THROUGH REAL ESTATE INVESTMENT.
+            </p>
+          </div>
+          <div className="w-full p-4 flex gap-1">
+            <div className="w-full p-2 bg-gray-100">
+              <h1 className="text-sm lg:text-lg font-medium">Project</h1>
+              <p className="text-base lg:text-base font-bold">Real Estate</p>
+            </div>
+            <div className="w-full p-2 bg-gray-100">
+              <h1 className="text-sm lg:text-lg font-medium">Best Return</h1>
+              <p className="text-base lg:text-base font-bold">60% (ROI)</p>
+            </div>
+            <div className="w-full p-2 bg-gray-100">
+              <h1 className="text-sm lg:text-lg font-medium">Timeline</h1>
+              <p className="text-base lg:text-base font-bold">1 Year</p>
+            </div>
+          </div>
+          <div>
+            <button className="bg-blue-950 text-white p-4 rounded-lg font-semibold">
+              Start Now
+            </button>
+          </div>
         </div>
-        <img
-          src="assets/tech-city.jpg"
-          alt="hero image"
-          className="object-contain w-full bg-blend-saturation"
-        />
+        <div className="w-full lg:w-4/5 h-full">
+          <img
+            src="assets/why-invest.png"
+            alt="hero image"
+            className="object-cover w-full h-full"
+          />
+        </div>
       </div>
     </section>
   );
--- src/components/containers/Navbar.jsx
+++ src/components/containers/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="w-full p-4 md:py-4 md:px-16 flex justify-between items-center absolute top-0 left-0 z-50">
-      <img src="assets/logo2.png" width={64} height={64} />
+    <nav className="w-full p-4 md:py-4 md:px-16 flex justify-between items-center sticky top-0 left-0 z-50 bg-white">
+      <img src="assets/logo2.png" width={80} height={80} />
       <ul className="hidden lg:flex items-center gap-4">
         {navLinks.map((link) => (
           <li key={link.label}>
@@ -38,7 +38,7 @@ const Navbar = () => {
           {showMenu ? <RiCloseLine size={24} /> : <RiMenu3Line size={24} />}
         </div>
         {showMenu && (
-          <ul className="absolute top-24 right-4 bg-blue-200 rounded-md p-4 w-44 slide-top">
+          <ul className="w-full absolute top-28 right-0 bg-white rounded-md p-4 slide-top">
             {navLinks.map((link, idx) => (
               <li className="text-end p-1" key={idx}>
                 <a
@@ -49,6 +49,15 @@ const Navbar = () => {
                 </a>
               </li>
             ))}
+            <li className="text-end p-1 mt-6">
+              <a
+                href="assets/tech-city-brochure.pdf"
+                className="p-2 text-base bg-blue-900 rounded-md text-gray-100 cursor-pointer hover:bg-blue-950 transition-all"
+                download
+              >
+                Brochure
+              </a>
+            </li>
           </ul>
         )}
       </div>
